c760ec61cdc5aa635cee027b3a38649efc17636b
sevQs <qs230104@gmail.com>
2023-04-29 19:07:54 +0900

    2023-04-29-000-end

--- app/src/main/java/com/example/codelab____composebasiclayout/MainActivity.kt
+++ app/src/main/java/com/example/codelab____composebasiclayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.codelab____composebasiclayout
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,14 +15,16 @@ import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.*
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -49,51 +52,98 @@ class MainActivity : ComponentActivity() {
 
             CodeLab____ComposeBasicLayoutTheme {
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
 
-                    Column() {
-
-                        SearchBar()
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        val list : MutableList<AlignYourBody> = mutableListOf()
-
-                        list.add(AlignYourBody(R.drawable.ab1_inversions, R.string.ab1_inversions))
-                        list.add(AlignYourBody(R.drawable.ab2_quick_yoga, R.string.ab2_quick_yoga))
-                        list.add(AlignYourBody(R.drawable.ab3_stretching, R.string.ab3_stretching))
-                        list.add(AlignYourBody(R.drawable.ab4_tabata, R.string.ab4_tabata))
-                        list.add(AlignYourBody(R.drawable.ab5_hiit, R.string.ab5_hiit))
-                        list.add(AlignYourBody(R.drawable.ab6_pre_natal_yoga, R.string.ab6_pre_natal_yoga))
-
-                        AlignYourBodyRow(alignYourBodyList = list)
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        val listOfFC = mutableListOf<FavoriteCollections>()
-
-                        listOfFC.add(FavoriteCollections(R.drawable.fc1_short_mantras, R.string.fc1_short_mantras))
-                        listOfFC.add(FavoriteCollections(R.drawable.fc2_nature_meditations, R.string.fc2_nature_meditations))
-                        listOfFC.add(FavoriteCollections(R.drawable.fc3_stress_and_anxiety, R.string.fc3_stress_and_anxiety))
-                        listOfFC.add(FavoriteCollections(R.drawable.fc4_self_massage, R.string.fc4_self_massage))
-                        listOfFC.add(FavoriteCollections(R.drawable.fc5_overwhelmed, R.string.fc5_overwhelmed))
-                        listOfFC.add(FavoriteCollections(R.drawable.fc6_nightly_wind_down, R.string.fc6_nightly_wind_down))
-
-                        FavoriteCollectionsGrid(favoriteCollectionsList = listOfFC)
-
-
-                    }
-
-                }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
+fun CBLApp () {
+
+    CodeLab____ComposeBasicLayoutTheme {
+
+        androidx.compose.material.Scaffold(
+            bottomBar = { CBLBottomNavigation() }
+        ) {  paddingValues ->
+
+            HomeScreen(Modifier.padding(paddingValues))
+
+        }
+
+    }
+}
+
+@Composable
+fun HomeScreen (modifier: Modifier = Modifier) {
+
+    Column(modifier) {
+
+        val list : MutableList<AlignYourBody> = mutableListOf()
+
+        list.add(AlignYourBody(R.drawable.ab1_inversions, R.string.ab1_inversions))
+        list.add(AlignYourBody(R.drawable.ab2_quick_yoga, R.string.ab2_quick_yoga))
+        list.add(AlignYourBody(R.drawable.ab3_stretching, R.string.ab3_stretching))
+        list.add(AlignYourBody(R.drawable.ab4_tabata, R.string.ab4_tabata))
+        list.add(AlignYourBody(R.drawable.ab5_hiit, R.string.ab5_hiit))
+        list.add(AlignYourBody(R.drawable.ab6_pre_natal_yoga, R.string.ab6_pre_natal_yoga))
+
+
+
+        val listOfFC = mutableListOf<FavoriteCollections>()
+
+        listOfFC.add(FavoriteCollections(R.drawable.fc1_short_mantras, R.string.fc1_short_mantras))
+        listOfFC.add(FavoriteCollections(R.drawable.fc2_nature_meditations, R.string.fc2_nature_meditations))
+        listOfFC.add(FavoriteCollections(R.drawable.fc3_stress_and_anxiety, R.string.fc3_stress_and_anxiety))
+        listOfFC.add(FavoriteCollections(R.drawable.fc4_self_massage, R.string.fc4_self_massage))
+        listOfFC.add(FavoriteCollections(R.drawable.fc5_overwhelmed, R.string.fc5_overwhelmed))
+        listOfFC.add(FavoriteCollections(R.drawable.fc6_nightly_wind_down, R.string.fc6_nightly_wind_down))
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        SearchBar(modifier = Modifier.padding(horizontal = 16.dp))
+
+        HomeSection(title =R.string.align_your_body ) {
+            AlignYourBodyRow(alignYourBodyList = list)
+        }
+
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid(favoriteCollectionsList = listOfFC )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+    }
+}
+
+@Composable
+fun HomeSection (
+
+    @StringRes title : Int,
+    modifier: Modifier = Modifier,
+    content : @Composable () -> Unit
+) {
+
+    Column(modifier) {
+
+        Text(
+            text = stringResource(title).uppercase(java.util.Locale.getDefault()),
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+
+
+        content()
+    }
+}
+
+@Composable
+
 fun SearchBar (
     modifier: Modifier = Modifier
 ) {
@@ -226,43 +276,47 @@ fun FavoriteCollectionsGrid(
     }
 }
 
+@Composable
+private fun CBLBottomNavigation(modifier: Modifier = Modifier) {
+
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colorScheme.surface,
+        modifier= modifier) {
+
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(id = R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = { }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(id = R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = { }
+        )
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CodeLab____ComposeBasicLayoutTheme {
-        
-        Column() {
 
-            SearchBar()
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            val list : MutableList<AlignYourBody> = mutableListOf()
-
-            list.add(AlignYourBody(R.drawable.ab1_inversions, R.string.ab1_inversions))
-            list.add(AlignYourBody(R.drawable.ab2_quick_yoga, R.string.ab2_quick_yoga))
-            list.add(AlignYourBody(R.drawable.ab3_stretching, R.string.ab3_stretching))
-            list.add(AlignYourBody(R.drawable.ab4_tabata, R.string.ab4_tabata))
-            list.add(AlignYourBody(R.drawable.ab5_hiit, R.string.ab5_hiit))
-            list.add(AlignYourBody(R.drawable.ab6_pre_natal_yoga, R.string.ab6_pre_natal_yoga))
-
-            AlignYourBodyRow(alignYourBodyList = list)
-
-            Spacer(modifier = Modifier.height(16.dp))
-            
-            val listOfFC = mutableListOf<FavoriteCollections>()
-            
-            listOfFC.add(FavoriteCollections(R.drawable.fc1_short_mantras, R.string.fc1_short_mantras))
-            listOfFC.add(FavoriteCollections(R.drawable.fc2_nature_meditations, R.string.fc2_nature_meditations))
-            listOfFC.add(FavoriteCollections(R.drawable.fc3_stress_and_anxiety, R.string.fc3_stress_and_anxiety))
-            listOfFC.add(FavoriteCollections(R.drawable.fc4_self_massage, R.string.fc4_self_massage))
-            listOfFC.add(FavoriteCollections(R.drawable.fc5_overwhelmed, R.string.fc5_overwhelmed))
-            listOfFC.add(FavoriteCollections(R.drawable.fc6_nightly_wind_down, R.string.fc6_nightly_wind_down))
-
-            FavoriteCollectionsGrid(favoriteCollectionsList = listOfFC)
-
-
-        }
-       
+        CBLApp()
     }
 }
